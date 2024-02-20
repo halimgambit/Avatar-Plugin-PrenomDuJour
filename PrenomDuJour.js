8c06d6dd1ce9f1bbaf44954f97dd173244e1d669
halimgambit <70286759+halimgambit@users.noreply.github.com>
2024-02-20 13:14:35 +0100

    Update PrenomDuJour.js

--- PrenomDuJour.js
+++ PrenomDuJour.js
@@ -7,16 +7,25 @@ exports.action = function(data, callback){
  
 }
 
-function prenomJour (data, client) {
-	
-	fetch('https://nameday.abalin.net/api/V1/today')
-    .then(response => response.json())
-    .then(reponse2 =>  {
-    Avatar.speak(`Aujoud'hui nous fétons le prénom de ${reponse2.nameday.fr}`, data.client, () => {
-	Avatar.Speech.end(data.client);
-	});
-})
-	
+async function prenomJour(data, client) {
+    try {
+        const response = await fetch('https://nameday.abalin.net/api/V1/today');
+
+        if (!response.ok) {
+            throw new Error(`Erreur lors de la requête : ${response.statusText}`);
+        }
+
+        const reponse2 = await response.json();
+        await new Promise((resolve) => {
+            Avatar.speak(`Aujourd'hui, nous fêtons le prénom de ${reponse2.nameday.fr}`, data.client, () => {
+                Avatar.Speech.end(data.client);
+                resolve();
+            });
+        });
+
+    } catch (error) {
+        console.error(`Erreur dans la fonction prenomJour : ${error.message}`);
+    }
 }
 
 function setClient (data) {
